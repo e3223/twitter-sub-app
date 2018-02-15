6619d1f048bf5201d8a298a6373898405ee954e7
Oleg Antonenko <metalsnow@ex.ua>
2018-02-15 19:59:56 +0200

    small fix

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -2,8 +2,10 @@
 
 namespace App\Http\Controllers;
 
+use App\User;
 use Twitter;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Redirect;
 
 class PostController extends Controller
 {
@@ -11,11 +13,14 @@ class PostController extends Controller
     public function postListCreateAll(Request $request)
     {
         if (isset($_POST['create'])) {
+            $user = new User();
+            $data = $request->all();
+            if ($user->validate($data, User::$rulesOnUpload, User::$messages)){
             $screen_name = $request->file('users');
             $list_id     = $request->arr;
-            $handle = fopen($screen_name, 'r');
-            $row    = 0;
-            $col    = 0;
+            $handle      = fopen($screen_name, 'r');
+            $row         = 0;
+            $col         = 0;
             if ($handle) {
                 while (($row = fgetcsv($handle, 1000)) !== false) {
                     foreach ($row as $k => $value) {
@@ -26,23 +31,32 @@ class PostController extends Controller
                 }
                 fclose($handle);
             }
-            $trim = array_map('trim', $results);
+            $trim   = array_map('trim', $results);
             $result = array_values( array_filter($trim) );
-            $data = array_chunk($result, 100);
-            $i    = 0;
-                while ($i < count($data)) {
+            $data   = array_chunk($result, 100);
+            $i      = 0;
+
+            while ($i < count($data)) {
                 $new = $data[$i];
                 Twitter::postListCreateAll(['list_id' => $list_id, 'screen_name' => $new]);
                 $i++;
             }
-            $request->session()->flash('alert-success',
-                'Users successfully added. ');
+            
+            $request->session()->flash('alert-success', 'Users successfully added. ');
             return redirect('user-lists');
-        } elseif (isset($_POST['delete'])) {
+         }else{
+          $errors = $user->errors();
+            return Redirect::back()
+              ->withErrors($errors)
+              ->withInput();
+         }
+         
+         
+            
+         } elseif (isset($_POST['delete'])) {
             $list_id = $request->arr;
             Twitter::destroyList(['list_id' => $list_id]);
-            $request->session()->flash('alert-success',
-                'List was successfully deleted ');
+            $request->session()->flash('alert-success', 'List was successfully deleted ');
             return redirect('user-lists');
         }
     }
@@ -52,9 +66,18 @@ class PostController extends Controller
         $list_name = $request->name;
         $list_mode = $request->public;
         $list_desc = $request->desc;
+        $data = $request->all();
+        $user = new User();
+        if ($user->validate($data, User::$rulesCreate, User::$messages)){
         Twitter::postList(['name' => $list_name, 'mode' => $list_mode, 'description' => $list_desc]);
-        $request->session()->flash('alert-success',
-            'New list was successfully created. ');
+        $request->session()->flash('alert-success', 'New list was successfully created. ');
             return redirect('user-lists');
+            }
+        else{
+          $errors = $user->errors();
+            return Redirect::back()
+              ->withErrors($errors)
+              ->withInput();
+      }
     }
 }
